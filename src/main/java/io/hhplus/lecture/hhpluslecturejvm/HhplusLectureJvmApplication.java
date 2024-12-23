404e9cfe85d53bb2efda9dee630a47294495f7e0
leesangjun <hubls@naver.com>
2024-12-23 20:45:38 +0900

    first commit

---
+++ src/main/java/io/hhplus/lecture/hhpluslecturejvm/HhplusLectureJvmApplication.java
@@ -0,0 +1,13 @@
+package io.hhplus.lecture.hhpluslecturejvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusLectureJvmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhplusLectureJvmApplication.class, args);
+    }
+
+}
